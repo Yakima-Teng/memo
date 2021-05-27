71eb0b90dc85e988709e4d18bdc338b4312f17ff
Yakima <cleveryun@163.com>
2021-05-28 02:16:45 +0800

    add proxy

--- packages/client/build/deploy-gh-pages.sh
+++ packages/client/build/deploy-gh-pages.sh
@@ -7,4 +7,5 @@ cd ./docs/.vuepress/dist || exit
 git init
 git add -A
 git commit -m 'update book'
+export http_proxy="http://127.0.0.1:1087"
 git push -f https://github.com/Yakima-Teng/memo.git master:gh-pages
